882d9be92263cc2f643c42a2778fe82db0e54b88
Juan Delgado <juandelgadog98@gmail.com>
2024-02-01 22:52:07 -0500

    :sparkles: feat(Initial commit): Start project

---
+++ src/data/resume-data.tsx
@@ -0,0 +1,233 @@
+import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+
+export const RESUME_DATA = {
+  name: "Juan David Delgado",
+  initials: "JDD",
+  location: "Cali, Colombia 🇨🇴",
+  locationLink: "https://maps.app.goo.gl/WQerjz7JRK2s6CbA8",
+  about:
+    "Passionate Informatics Engineer and Software Developer skilled in NodeJs/JavaScript/TypeScript",
+  summary:
+    "I'm Informatics Engineer and Software Developer specializing in backend development. Proficient in JavaScript/TypeScript, I work with frameworks like Express.js and Nest.js, working with ORM such as TypeORM. My expertise extends to SQL databases (MySQL, PostgreSQL) and NoSQL (MongoDB). I am skilled in cloud computing, working with tools like AWS (EC2, Lambdas, S3), Linux, Bash, and Git. I also have experience in frontend development with React and Next.js.",
+  avatarUrl: "https://lh3.googleusercontent.com/fife/AGXqzDkvDDS4R2lbWdV_vgwsx_mlvum-YodVFSD7DXV1iqlB_jEvMbQijd_LjSjWlTE0F5LAIAA2MULpPve-y4ZtgDLCE3Cws1O_9cfzh3kmqMBCZugsdN4SQv19b_3hwOf29Qbj_iNTYoN4uaikaQqkjlwJuFexvGtzA0p0--uX3_fJmNo6qmNotDFXQIEty5da61Lun85COWeYT4CBESjouqNl4P9Ba7Fl3dkAZJ1BTLf4a2YCaG4mnMpDwnk3RA5WF-HDSKGAfV6o-0Kk47RoSj1gyWZzJv5Z1KywEO_8ti3uuAhRC1-5exthAPvKNfRv62I-xyv0rt0vZ6_UEmQZY0fX5iuT8PVAnTwBI0nOO9Uq8zcGTBUIXWkDTUJcXi9OHQ4lV7MIf20yG2pTT6Q-cHbcURE8zTLQavAQE9svUgkY0AaFZvqJYHe3X-ViNNrmHnIRWHp-thL8FBq8UiOQwY9moUq3ApvrCn4aArNoq5XXtrRx8si8yNgAfkHVl2xBH__1dJLEhBO2TF1WIbTWQJnhnJkxFjD9LZus8RgRqj9JnuvvSmxYzdkLckquFHM6LhMY6-Zfp9kgfnIHY6w2bl4wCouvo0LHlbG04HHYmTytfSJrw_nevnqxe7oszty29zL_16lctkM0Z3nV08RSG-4NrMIrdKZyzwmWG34e-eIYkGdpaifmKr9O78nwpAYkxtTYKPZe9BichAv9GnIDxcu5Ej1a0jAYSsIMEBM5BQkymZlPDDkwC0YAaM4maFyhILxMkuMkB6VPsi0Brn8HCSfqkEGwEuQpaPK2h9Cvk-ccY2VnfKjVU3TbPG8fIrmS1euu17uJDLftjAcvXoZia42mFURweygh1-4993GRlwVz79aDIh4o-ZA5t7vlaU5FeZHT3MAm3FhPmkq9Uepeq-5Fb1PrXXgvRxJD5vkHkXhFoIY_Mq9i9DbTIOQOcl17LyJCij94pHfywQnm7Exhbc2oScBI1NC7lLeIMTTRJzj13BTGOlILf98kDPQfgK-UZtN4grfBKOJrfsSRz-5qWf7CJxRZokgVTdrh8QL_sCfU8mMFVBi9LkG2VoMD9RR6lBxj3hiddLZgS-IQKXO0jBXa0hJcXOpyNH81ryOI05HvWe-rVgaoVzdnUhF7eheKXeBDN2Di4jhgEovNXtmytI6daBcmo0VDl2pxOrkGvE_fKOTi_RwZAHqJYAiYGSOGPrrS9rI1vQr_rGmn3U4Ya3UczSEUJkZaqXr77qMFa1pxr6G8DdaoaEbLAGYefTzmgdJpvOPYcn1VpHDGO0LLjGVUoDLgykpuhPtNzofxRV6ms2K4Us4Xw4Su0VtceWJm1fOkSmRo9EeWtcgmLM-9twWow5shpw4Rw6_UXm9LIuQUaVvnBK4vSffjYpD7gwMWagCCGT34diXGXo4m3LxCPlasozyJa19wvFrLsPQwRsmtpL-S_B4ppt2m9cBdV1UptQq9y3hHi6IdnNq9DV8YA5mv9hzZc5q1VmxGGi2qjyl23Y9cAJ_mc47nkfsuI16VgGYW2Cqpw7U=w2258-h1606",
+  contact: {
+    email: "juandelgadog98@gmail.com",
+    tel: "+573176411890",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/JuanGuzman-io",
+        icon: GitHubIcon,
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/juan-david-delgado-guzmán",
+        icon: LinkedInIcon,
+      }
+    ],
+  },
+  personalWebsiteUrl: "https://juandadev.js",
+  education: [
+    {
+      school: "Universidad Autónoma de Occidente",
+      degree: "Informatics Engineer",
+      start: "2017",
+      end: "2023",
+    },
+    {
+      school: "Universidad Autónoma de Occidente",
+      degree: "Master Big Data Analytics",
+      start: "2023",
+      end: "Present",
+    },
+  ],
+  work: [
+    {
+      company: "AMA Publicidad",
+      link: "https://ama-programacion.xyz",
+      badges: ["Remote"],
+      title: "Freelancer - Full Stack Developer",
+      start: "Oct - 2021",
+      end: "Dec - 2021",
+      description:
+        `Development of the production chart to manage customers, orders, machine jobs, contact, and machinery. • Client side: React/Bootstrap. • Server side: NodeJS (Express) • Database: PostgreSQL • Cloud service: AWS (EC2) • Firebase Authentication (Google)`,
+    },
+    {
+      company: "Alliance Bioversity International and CIAT",
+      link: "https://github.com/AllianceBioversityCIAT",
+      badges: ["Hybrid"],
+      title: "Intern - Full Stack Developer:",
+      start: "Sep - 2022",
+      end: "Feb - 2023",
+      description:
+        `• Development of modules for applications (BackEnd - NestJs).
+        • Implementation of new functionalities, code clean, optimizations and applications performance improvement (Back End - ExpressJS).
+        • Database management, implementation of functions, views and procedures (MySQL).
+        • Implementation of unit testing and e2e testing.
+        • Provide support, access management and deck support for platform users.
+        • Contribution in the unit Notion for the documentation of projects and the creation of the internal directory.`,
+    },
+    {
+      company: "S.A.R.E.S. Foundation (University project)",
+      link: "https://sarescali.wixsite.com/fundacinsalvamentoyr",
+      badges: ["Remote"],
+      title: "BackEnd Developer",
+      start: "Jan 2023",
+      end: "Jun 2023",
+      description: "Development of the web application for inventory and supplies management for the foundation. Technologies: TypeScript, NestJs, MySQL, Amazon S3, Git, GitHub.",
+    },
+    {
+      company: "Alliance Bioversity International and CIAT",
+      link: "https://github.com/AllianceBioversityCIAT",
+      badges: ["Hybrid"],
+      title: "Analyst - Full Stack Developer",
+      start: "Mar - 2023",
+      end: "Feb - 2024",
+      description:
+        `• Development of modules for applications (- BackEnd NestJs/ExpressJS - FrontEnd Angular).
+        • Database management, implementation of functions, views and procedures (MySQL).
+        • Provide support, access management and deck support for platform users.
+        • Contribution in the unit Notion for the documentation of projects and the creation of the internal directory.`,
+    },
+  ],
+  skills: [
+    "JavaScript",
+    "TypeScript",
+    "NodeJS",
+    "NestJs",
+    "BackEnd",
+    "REST API", 
+    "TypeORM",
+    "MySQL",
+    "PostgreSQL",
+    "MongoDB",
+    "AWS - S3, Lambda, EC2",
+    "Linux OS, Bash",
+    "React/Next.js",
+    "Documentation - JIRA, Notion",
+  ],
+  projects: [
+    {
+      title: "Daniela's Portfolio",
+      techStack: [
+        "JavaScript",
+        "React",
+        "Next.js",
+        "Sanity",
+        "Vercel"
+      ],
+      description: "Personal portfolio website for Daniela Gomez, Mechatronics Engineer and AI Developer.",
+      link: {
+        label: "daniela-portfolio.com",
+        href: "daniela-portfolio.vercel.app",
+      }
+    },
+    {
+      title: "Banco Textil App",
+      techStack: [
+        "Java",
+        "Android",
+        "Firebase",
+        "Firestore",
+        "Android Studio"
+      ],
+      description: "Application developed in Java programming language, the entire design is based on MaterialDesign parameters, making it consistent and subtle.",
+      link: {
+        label: "project-mobile-app.com",
+        href: "https://github.com/JuanGuzman-io/BancoTextil.git",
+      }
+    },
+    {
+      title: "Reporting tool (CGIAR app)",
+      techStack: [
+        "TypeScript",
+        "NestJs",
+        "TypeORM",
+        "MySQL",
+        "Angular",
+        "AWS Lambda",
+      ],
+      description: "Maintenance developer, improving code and new functionalities. Manage the database, implement functions, views and procedures.",
+      link: {
+        label: "reporting.cgiar.org",
+        href: "https://github.com/AllianceBioversityCIAT/onecgiar_pr.git",
+      }
+    },
+    {
+      title: "Fundacion S.A.R.E.S. App",
+      techStack: [
+        "NestJs",
+        "MySQL",
+        "SMTP",
+        "Railway",
+        "Cloud computing"
+      ],
+      description: "Development of the web application for inventory and supplies management for the foundation.",
+      link: {
+        label: "fundacion-sares.com",
+        href: "https://github.com/PIRLO-team/fundacion-sares.git",
+      }
+    },
+    {
+      title: "QA Platform (CGIAR app)",
+      techStack: [
+        "TypeScript",
+        "ExpressJs",
+        "TypeORM",
+        "MySQL",
+        "Angular",
+        "AWS Lambda",
+      ],
+      description: "Maintenance developer, improving code and new functionalities. Manage the database, implement functions, views and procedures.",
+      link: {
+        label: "qa.cgiar.org",
+        href: "https://github.com/AllianceBioversityCIAT/QA.git",
+      }
+    },
+    {
+      title: "ToC Integration API (CGIAR app)",
+      techStack: [
+        "TypeScript",
+        "ExpressJs",
+        "MySQL",
+        "AWS Lambda",
+      ],
+      description: "Maintenance developer, improving code and new functionalities. Manage the database.",
+      link: {
+        label: "lambda-toc.clarisa.cgiar.org",
+        href: "https://github.com/AllianceBioversityCIAT/CLARISA.git",
+      }
+    },
+    {
+      title: "AMA Publicidad App",
+      techStack: [
+        "JavaScript",
+        "ExpressJs",
+        "PostgreSQL",
+        "React",
+        "Firebase",
+        "EmailJs",
+      ],
+      description: "Development of the production chart to manage customers, orders, machine jobs, contact, and machinery.",
+      link: {
+        label: "music-player.com",
+        href: "https://github.com/JuanGuzman-io/music_player.git",
+      }
+    },
+    {
+      title: "Music player app",
+      techStack: [
+        "JavaScript",
+        "ExpressJs",
+        "PostgreSQL",
+        "React",
+        "Frame motion",
+      ],
+      description: "Music player app developed in JavaScript programming language.",
+      link: {
+        label: "music-player.com",
+        href: "https://github.com/JuanGuzman-io/music_player.git",
+      }
+    },
+  ],
+} as const;
